95995801edd7822ab636e397d27b574a3f9e116a
aSamurai <xhc044x@gmail.com>
2017-09-13 02:51:42 +0300

    Add new unit tests for Button component

--- quiz_frontend/src/components/ui/tests/button.test.js
+++ quiz_frontend/src/components/ui/tests/button.test.js
@@ -4,20 +4,35 @@ import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
 describe('Button component test', () => {
+  const expectedDefaultProps = {
+    title: 'Button',
+    className: 'button'
+  };
   it('Button title renders correctly without props', () => {
     const button = shallow(
       <Button/>
     );
     const tree = toJson(button);
+
+    expect(button.props().className).toBe(expectedDefaultProps.className);
+    expect(button.instance().props.title).toBe(expectedDefaultProps.title);
+    expect(button.props().children).toBe(expectedDefaultProps.title);
     expect(tree).toMatchSnapshot();
   }); 
   it('Button title renders correctly with props', () => { 
+    const dummyProps = {
+      title: 'subscribe'
+    }
     const button = shallow(
       <Button
-          title="subscribe"
+          title={dummyProps.title}
       />
     );
     const tree = toJson(button);
+
+    expect(button.props().className).toBe(expectedDefaultProps.className);
+    expect(button.instance().props.title).toBe(dummyProps.title);
+    expect(button.props().children).toBe(dummyProps.title);
     expect(tree).toMatchSnapshot();
   }); 
 });
