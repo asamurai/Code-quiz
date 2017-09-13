caa7f7952f910a7e44de1b92643854d879e03971
aSamurai <xhc044x@gmail.com>
2017-09-13 03:28:28 +0300

    Add Icon component to Button component

--- quiz_frontend/src/components/ui/button/index.jsx
+++ quiz_frontend/src/components/ui/button/index.jsx
@@ -1,28 +1,81 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Icon } from './../index.js';
 
 import styles from './index.sass';
 
 class Button extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            iconLeft: this.props.iconLeft,
+            iconRight: this.props.iconRight
+        };
+    }
+
+    
+    componentWillMount (){
+        const { iconLeft, iconRight } = this.props;
+        switch (true) {
+            case (!iconLeft && iconRight):
+                this.setState({
+                    iconLeft: false,
+                    iconRight: true                    
+                });
+                break;
+            case (iconLeft && !iconRight):
+                this.setState({
+                    iconLeft: true,
+                    iconRight: false                    
+                });                
+                break;
+            case (iconLeft && iconRight) || (!iconLeft && !iconRight):
+            default:
+                this.setState({
+                    iconLeft: true,
+                    iconRight: false               
+                });
+                console.warn('Params iconLeft and iconRight should not be the same.');
+                break;
+        }
+    }
+
     render () {
-        const { title, ...rest } = this.props;
+        const { title, icon,...rest } = this.props;
+        const { iconLeft, iconRight } = this.state;
         return (
             <div
                 className={styles.button}
+                icon={icon}
                 {...rest}
             >   
+                {icon && iconLeft &&
+                    <Icon
+                        icon={icon}
+                    />
+                }
                 {title}
+                {icon && iconRight &&
+                    <Icon
+                        icon={icon}
+                    />
+                }
             </div>
         );
     }
 }
 
 Button.propTypes = {
-    title: PropTypes.string
+    title: PropTypes.string,
+    icon: PropTypes.string,
+    iconLeft: PropTypes.bool,
+    iconRight: PropTypes.bool
 };
 
 Button.defaultProps = {
-    title: 'Button'
+    title: 'Button',
+    iconLeft: true,
+    iconRight: false
 };
 
 export default Button;
--- quiz_frontend/src/components/ui/tests/button.test.js
+++ quiz_frontend/src/components/ui/tests/button.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Button } from '../index.js';
+import { Button, Icon } from '../index.js';
+import icons from './../../../utils/icons.json';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
@@ -14,25 +15,36 @@ describe('Button component test', () => {
     );
     const tree = toJson(button);
 
+    expect(button.find('svg').length).toBe(0);
     expect(button.props().className).toBe(expectedDefaultProps.className);
     expect(button.instance().props.title).toBe(expectedDefaultProps.title);
-    expect(button.props().children).toBe(expectedDefaultProps.title);
+    expect(button.props().children.filter(el=>el!=undefined)[0]).toBe(expectedDefaultProps.title);
     expect(tree).toMatchSnapshot();
   }); 
   it('Button title renders correctly with props', () => { 
     const dummyProps = {
-      title: 'subscribe'
-    }
+      title: 'subscribe',
+      icon: icons.facebook,
+      iconRight: true,
+      iconLeft: false
+    };
     const button = shallow(
       <Button
           title={dummyProps.title}
+          icon={dummyProps.icon}
+          iconRight={dummyProps.iconRight}
+          iconLeft={dummyProps.iconLeft}
       />
     );
     const tree = toJson(button);
-
+    
+    expect(button.find(Icon).length).toBe(1);
+    expect(button.instance().props.icon).toBe(dummyProps.icon);
+    expect(button.instance().props.iconRight).toBe(dummyProps.iconRight);
+    expect(button.instance().props.iconLeft).toBe(dummyProps.iconLeft);
     expect(button.props().className).toBe(expectedDefaultProps.className);
     expect(button.instance().props.title).toBe(dummyProps.title);
-    expect(button.props().children).toBe(dummyProps.title);
+    expect(button.props().children.filter(child=>typeof(child)==='string')[0]).toBe(dummyProps.title);
     expect(tree).toMatchSnapshot();
   }); 
 });
