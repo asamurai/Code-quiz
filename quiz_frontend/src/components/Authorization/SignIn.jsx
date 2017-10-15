af1f70d0e7d52ae0390a946baad4d63a9b522ae4
aSamurai <xhc044x@gmail.com>
2017-10-16 00:20:22 +0300

    Add UI form gor sign in

--- quiz_frontend/src/components/Authorization/SignIn.jsx
+++ quiz_frontend/src/components/Authorization/SignIn.jsx
@@ -1,13 +1,68 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { 
+    Form,
+    Icon, 
+    Input, 
+    Button, 
+    Checkbox 
+} from 'antd';
+const FormItem = Form.Item;
 
-class SignIn extends Component {
-    render () {
+import * as routes from './../../routes';
+
+import './signin.css';
+
+class NormalLoginForm extends React.Component {
+    constructor(props){
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleSubmit(e){
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+            console.log('Received values of form: ', values);
+            }
+        });
+    }
+    render() {
+        const { getFieldDecorator } = this.props.form;
         return (
-            <div>
-                Sign In
-            </div>
+            <Form onSubmit={this.handleSubmit} className="login-form">
+                <FormItem>
+                    {getFieldDecorator('userName', {
+                    rules: [{ required: true, message: 'Please input your username!' }],
+                    })(
+                    <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="Username" />
+                    )}
+                </FormItem>
+                <FormItem>
+                    {getFieldDecorator('password', {
+                    rules: [{ required: true, message: 'Please input your Password!' }],
+                    })(
+                    <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="Password" />
+                    )}
+                </FormItem>
+                <FormItem>
+                    {getFieldDecorator('remember', {
+                    valuePropName: 'checked',
+                    initialValue: true,
+                    })(
+                    <Checkbox>Remember me</Checkbox>
+                    )}
+                    <a className="login-form-forgot" href="">Forgot password</a>
+                    <Button type="primary" htmlType="submit" className="login-form-button">
+                    Log in
+                    </Button>
+                    {`Or `}  
+                    <Link to={routes.SIGNUP_PATH}>
+                    register now!
+                    </Link>
+                </FormItem>
+            </Form>
         );
     }
 }
 
-export default SignIn;
+export default Form.create()(NormalLoginForm);
