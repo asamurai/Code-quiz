10157043e4745f7dd1dd51dee8b93a2290c2436d
bohdan <belokur.bogdan@yandex.ru>
2017-10-26 22:26:34 +0300

    Fixed activate email output. Correct error output

--- quiz_backend/profiles/models.py
+++ quiz_backend/profiles/models.py
@@ -2,7 +2,6 @@ import datetime
 
 from django.conf import settings
 from django.db import models
-from django.utils.timezone import now as datetime_now
 
 
 class RegistrationProfile(models.Model):
@@ -14,13 +13,3 @@ class RegistrationProfile(models.Model):
 
     user = models.OneToOneField(settings.AUTH_USER_MODEL, unique=True, verbose_name='user', related_name='api_registration_profile')
     activation_key = models.CharField(max_length=40)
-
-    def activation_key_expired(self):
-
-        from . import utils
-
-        expiration_date = datetime.timedelta(
-            days=utils.get_settings('REGISTRATION_API_ACCOUNT_ACTIVATION_DAYS'))
-        return self.activation_key == self.ACTIVATED or \
-               (self.user.date_joined + expiration_date <= datetime_now())
-
