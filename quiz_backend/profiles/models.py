f052187804d90d8d647f1056f1ad4ca738f621ec
bohdan <belokur.bogdan@yandex.ru>
2017-10-30 00:27:22 +0200

    Adding bio, initial commit with UserProfile serializer and models

--- quiz_backend/profiles/models.py
+++ quiz_backend/profiles/models.py
@@ -13,3 +13,10 @@ class RegistrationProfile(models.Model):
 
     user = models.OneToOneField(settings.AUTH_USER_MODEL, unique=True, verbose_name='user', related_name='api_registration_profile')
     activation_key = models.CharField(max_length=40)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, unique=True, verbose_name='user',
+                                related_name='extended_profile')
+    bio = models.TextField()
+    profile_image = models.ImageField(blank=True, null=True)
--- quiz_backend/profiles/serializers.py
+++ quiz_backend/profiles/serializers.py
@@ -1,17 +1,23 @@
 # from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from rest_framework import serializers
-
+from .models import UserProfile
 
 class UserSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = User
-        print(model)
         fields = ('username', 'email', 'password')
 
-    def to_native(self, obj):
-        """Remove password field when serializing an object"""
-        ret = super(UserSerializer, self).to_native(obj)
-        del ret['password']
-        return ret
+
+class UserBaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id','username', 'email', 'first_name', 'last_name')
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    user = UserBaseSerializer()
+
+    class Meta:
+        model = UserProfile
+        fields = ('user, 'profile_image', 'bio')
