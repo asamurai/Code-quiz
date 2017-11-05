cbd05280690e8ec656cc0991452eb2177be1d5a6
aSamurai <xhc044x@gmail.com>
2017-11-06 01:06:55 +0200

    Add mock data for detailed info component

--- quiz_frontend/src/components/FullQuizzes/QuizDetailedInfo/index.js
+++ quiz_frontend/src/components/FullQuizzes/QuizDetailedInfo/index.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
 
+const mockTestData = {
+    id: 1,
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS45PBU2qyXzci5sfj0Dm4C_4V87mnpvVCqbjyZOHFQ_aROJwYl',
+    title: 'React',
+    content: 'React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.'
+};
+
 class QuizDetailedInfo extends Component {
     render () {
         return (
-            <div>
-                Detailed Info about quiz and technology
+            <div
+                style={{
+                    maxWidth: '700px',
+                    margin: '0 auto',
+                    padding: '30px'
+                }}
+            >
+                <h3>{mockTestData.title}</h3>
+                <div
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        alignItems: 'center'
+                    }}
+                >
+                    <div
+                        style={{
+                            maxWidth: '300px'
+                        }}
+                    >
+                        {mockTestData.content}
+                    </div>
+                    <div>
+                        <img
+                            src={mockTestData.image}
+                        />
+                    </div>
+                </div>
             </div>
         );
     }
