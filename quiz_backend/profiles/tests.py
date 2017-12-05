12a4982914326e813841ee394c619b632a3440e9
whereeaglesdare <belokur.bogdan@yandex.ru>
2017-12-05 03:27:46 +0200

    Adding tests for /profiles/

--- quiz_backend/profiles/tests.py
+++ quiz_backend/profiles/tests.py
@@ -1,34 +1,44 @@
 from django.test import TestCase
 
-from rest_framework.test import APIRequestFactory
-from rest_framework.test import force_authenticate
+from rest_framework.test import RequestsClient
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
+from django.test.utils import override_settings
+
 
 from .models import UserProfile
 
 
-class UsersUpdate(TestCase):
+class UsersCRUD(TestCase):
 
-    def test_put(self):
+    def setUp(self):
+        user_model = get_user_model()
+        new_user = {'username': 'test_user', 'password': 'verylongpassword', 'email': 'email@mail.ru'}
+        self.user = user_model.objects.create_user(**new_user)
+        self.user.is_active = True
+        self.user.save()
+        self.obj = UserProfile.objects.create(user=self.user)
+        self.obj.save()
+
+    @override_settings(DEBUG=True)
+    def test_put_users(self):
         '''
         Test for /PUT/ users request. Add image in this path to test
         '''
-        user_model = get_user_model()
-        new_user = {'user':'test_user', 'password': 'verylongpassword'}
-        user = user_model.objects.create_user(**new_user)
-        user.is_active = True
-        user.save()
-        UserProfile.objects.create(user=user)
-        token, created = Token.objects.get_or_create(user='test_user')
-        factory = APIRequestFactory()
-
+        token, created = Token.objects.get_or_create(user=self.user)
+        client = RequestsClient()
         user = User.objects.get(id=1)
-        with open('image.jpg') as image_file:
-            data = {"user_id":"4","email":"email@email.ru","bio":"value2","username":"value1",
-                "first_name":"boba","last_name":"deb", "profile_image": image_file}
+        data = {"user_id":"4","email":"email@email.ru","bio":"value2","username":"value1",
+                "first_name":"boba","last_name":"deb"}
+        headers = {"Authorization": "Token {}".format(token)}
+        request = client.put('http://testserver/user/id/1/', headers=headers, data=data,
+                             files={'profile_image': open('image.jpg', 'rb')})
+        print(request.text)
+        self.assertEqual(request.status_code, 200)
 
-        request = factory.put('/user/4/',data=data)
-        force_authenticate(request, user=user, token=user.auth_token)
-        print(request.body)
+
+    def test_get(self):
+        client = RequestsClient()
+        request = client.get('http://testserver/user/id/1/')
+        print(request.text)
