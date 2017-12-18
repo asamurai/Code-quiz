529e42df26dfc493c75b465b01410acc46ff2753
aSamurai <xhc044x@gmail.com>
2017-12-18 03:29:01 +0200

    Add tests for HOC helpers

---
+++ quiz_frontend/src/helpers/hocHelpers.test.js
@@ -0,0 +1,32 @@
+import {
+    getValuesFromForm,
+    getCertainValuesFromForm
+} from './hocHelpers';
+
+const customForm = {
+    name: { value: '' },
+    phone: { value: '' }
+};
+
+describe('Hoc helpers test', () => {
+    it('getValuesFromForm helper test', () => {
+        expect(
+            getValuesFromForm(customForm)
+        ).toEqual(
+            {
+                name: '',
+                phone: ''
+            }
+        );
+    });
+    it('getCertainValuesFromForm helper test', () => {
+        const certaineListOfParams = ['name'];
+        expect(
+            getCertainValuesFromForm(customForm, certaineListOfParams)
+        ).toEqual(
+            {
+                name: ''
+            }
+        );
+    });
+});
