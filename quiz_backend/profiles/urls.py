808f11715a06abd71881c1c6665c83c4c4152850
whereeaglesdare <belokur.bogdan@yandex.ru>
2017-12-25 01:33:06 +0200

    New view for auth (post /login). changing list view for answers. Bugfixes

--- quiz_backend/profiles/urls.py
+++ quiz_backend/profiles/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 from rest_framework.authtoken import views
-from .views import register, activate, logout, restore_password, UserView
+from .views import register, activate, logout, restore_password, UserView, login
 
 urlpatterns = [
     url(r'^register/$', register),
     url(r'^activate/(?P<activation_key>\w+)/$', activate),
-    url(r'^api-token-auth/', views.obtain_auth_token),
+    url(r'^login/', login),
     url(r'^logout/$', logout),
     url(r'^forgot_password/$', restore_password),
     url(r'^user/id/(?P<id>\d+)/$', UserView.as_view()),
