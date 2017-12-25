a85b510430d1367d6c3a3b8ea9f95c03a974e9f8
whereeaglesdare <belokur.bogdan@yandex.ru>
2017-12-26 01:40:39 +0200

    AutoOneToOneField in extended users model

--- quiz_backend/profiles/models.py
+++ quiz_backend/profiles/models.py
@@ -2,7 +2,7 @@ import datetime
 
 from django.conf import settings
 from django.db import models
-
+from annoying.fields import AutoOneToOneField
 
 class RegistrationProfile(models.Model):
     """
@@ -11,13 +11,13 @@ class RegistrationProfile(models.Model):
     """
     ACTIVATED = u"ALREADY_ACTIVATED"
 
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, unique=True, verbose_name='user',
+    user = models.AutoOneToOneField(settings.AUTH_USER_MODEL, unique=True, verbose_name='user',
                                 related_name='api_registration_profile', on_delete=models.CASCADE,)
     activation_key = models.CharField(max_length=40)
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, unique=True, verbose_name='user_profile',
+    user = models.AutoOneToOneField(settings.AUTH_USER_MODEL, unique=True, verbose_name='user_profile',
                                 related_name='extended_profile', on_delete=models.CASCADE,)
     bio = models.TextField(null=True)
     profile_image = models.ImageField(null=True, upload_to='profile_images/')
