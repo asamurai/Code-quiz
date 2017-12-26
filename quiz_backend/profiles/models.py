e34257573b9090294a9ee3f1f77e61ede005b769
Artem Vergun <xhc044x@gmail.com>
2017-12-26 02:16:18 +0200

    Fix autoonetoonefield error

--- quiz_backend/profiles/models.py
+++ quiz_backend/profiles/models.py
@@ -11,13 +11,13 @@ class RegistrationProfile(models.Model):
     """
     ACTIVATED = u"ALREADY_ACTIVATED"
 
-    user = models.AutoOneToOneField(settings.AUTH_USER_MODEL, unique=True, verbose_name='user',
+    user = AutoOneToOneField(settings.AUTH_USER_MODEL, unique=True, verbose_name='user',
                                 related_name='api_registration_profile', on_delete=models.CASCADE,)
     activation_key = models.CharField(max_length=40)
 
 
 class UserProfile(models.Model):
-    user = models.AutoOneToOneField(settings.AUTH_USER_MODEL, unique=True, verbose_name='user_profile',
+    user = AutoOneToOneField(settings.AUTH_USER_MODEL, unique=True, verbose_name='user_profile',
                                 related_name='extended_profile', on_delete=models.CASCADE,)
     bio = models.TextField(null=True)
     profile_image = models.ImageField(null=True, upload_to='profile_images/')
